021836d5eebdb73735789502413a5629bd1f2264
lidoszka <lidkagandecka@gmail.com>
2018-06-04 16:47:15 +0200

    change in "SearchResults.js"

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -5,7 +5,7 @@ import TrackList from "../TrackList/TrackList";
 class SearchResults extends React.Component {
     render() {
         return (
-            <div className="SearchResults" searchResults="this.state.searchResults">
+            <div className="SearchResults">
             <h2>Results</h2>
             <TrackList tracks={this.props.searchResult} onAdd={this.props.onAdd} />
             </div>
