954d319eff5f73ad3787385af9ace3dd1c671c41
lidoszka <lidkagandecka@gmail.com>
2018-06-11 20:50:55 +0200

    last one

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -41,7 +41,6 @@ class App extends React.Component {
     savePlaylist() {
         const trackURIs = this.state.playlistTracks.map(track => track.uri)
         Spotify.savePlaylist(this.state.playlistName, trackURIs).then((playlist) => {
-            console.log(playlist);
         })
     }
         
